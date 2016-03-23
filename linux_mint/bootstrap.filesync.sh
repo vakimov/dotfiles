5e1db6b3e760d22128eaed8d5695e9a2d8360f2c
Victor Akimov <akimov.victor@gmail.com>
2016-03-24 01:44:35 +0300

    syncthing on strartup

--- linux_mint/bootstrap.filesync.sh
+++ linux_mint/bootstrap.filesync.sh
@@ -1,8 +1,8 @@
-if [[ $UID != 0 ]]; then
-    echo "Please run this script with sudo:"
-    echo "sudo $0 $*"
-    exit 1
-fi
+#if [[ $UID != 0 ]]; then
+#    echo "Please run this script with sudo:"
+#    echo "sudo $0 $*"
+#    exit 1
+#fi
 
 ## Setup Syncthing https://apt.syncthing.net/
 
@@ -16,5 +16,21 @@ echo "deb http://apt.syncthing.net/ syncthing release" | sudo tee /etc/apt/sourc
 sudo apt-get update
 sudo apt-get install syncthing
 
-# Settings
-sudo ln -s /home/vakimov/dotfiles/linux_mint/syncthing/syncthing.conf /etc/init/syncthing.conf
+# Startup for user
+# https://github.com/syncthing/syncthing/tree/master/etc/linux-upstart
+
+# Not working for --user Session Job
+#mkdir -p ~/.config/upstart/
+#ln -s /home/vakimov/dotfiles/linux_mint/syncthing/syncthing.conf ~/.config/upstart/
+
+sudo ln -s /home/vakimov/dotfiles/linux_mint/syncthing/syncthing.conf /etc/init/
+sudo initctl reload-configuration
+sudo initctl start syncthing
+
+# Not for linux mint with upstart (not systemd)
+# https://docs.syncthing.net/users/autostart.html#how-to-set-up-a-user-service
+#mkdir -p ~/.config/systemd/user/
+#sudo ln -s /home/vakimov/dotfiles/linux_mint/syncthing/syncthing.service ~/.config/systemd/user/syncthing.service
+#systemctl --user enable syncthing.service
+#systemctl --user start syncthing.service
+
