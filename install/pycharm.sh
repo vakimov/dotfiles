b701716993636eeddd87d729e52237023834c432
Victor <akimov.victor@gmail.com>
2017-11-05 22:51:58 +0300

    Add setting ru locale for pycharm

--- install/pycharm.sh
+++ install/pycharm.sh
@@ -3,3 +3,17 @@
 
 cd $DOTFILES_DIR/etc/pycharm/settings
 zip -r ../settings.jar *
+
+# Set Russian locale
+
+echo -n "Enter pycharm path and press [ENTER]: "
+read PYCHARM_PATH
+echo
+
+read -r -d '' LOCALE <<- EOM
+-Duser.country=RU
+-Duser.language=ru
+EOM
+
+echo "$LOCALE" >> "$PYCHARM_PATH"/bin/pycharm.vmoptions
+echo "$LOCALE" >> "$PYCHARM_PATH"/bin/pycharm64.vmoptions
