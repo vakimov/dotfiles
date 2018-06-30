df067cab51010bdb3f7fd71d57c9c2552cc691af
Victor Akimov <akimov.victor@gmail.com>
2018-06-30 11:17:41 +0300

    Fix setting up sublime on mac; Add scroll_past_end preference

--- install/mac/subl.sh
+++ install/mac/subl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-config_path="Library/Application\ Support/Sublime\ Text\ 3"
-package_user_path="Packages/User"
-cp "$DOTFILES_DIR/etc/sublime-text-3/$package_user_path/insert_datetime.py" \
+config_path=Library/Application\ Support/Sublime\ Text\ 3
+package_user_path=Packages/User
+rsync -r "$DOTFILES_DIR/etc/sublime-text-3/$package_user_path/" \
   "$HOME/$config_path/$package_user_path/"
