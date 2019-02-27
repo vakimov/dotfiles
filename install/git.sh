5817f1e12b8f3b7590272f15b92c914d34aedbef
Victor Akimov <akimov.victor@gmail.com>
2019-02-27 13:40:50 +0300

    Specify git diff tools for specific platform

--- install/git.sh
+++ install/git.sh
@@ -2,3 +2,5 @@
 ## Install Git
 
 sudo apt-get install -y git gitg bash-completion meld
+
+git config --global diff.external ~/.dotfiles/bin/meld_diff.py
--- install/mac/git.sh
+++ install/mac/git.sh
@@ -2,4 +2,12 @@
 ## Install Git
 
 brew install git && brew install bash-completion
-brew cask install meld
+
+# locate xcode utilities
+sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
+
+# Disable notification "file is not ASCII"
+defaults write "com.apple.FileMerge" maxNonAscii -int 2147483647
+
+git config --global merge.tool opendiff
+git config --global diff.external ~/.dotfiles/bin/opendiff-git.sh
