cd0f14eb6beb1c74dbfe049579ad1b13eba85199
Victor Akimov <akimov.victor@gmail.com>
2019-03-17 13:48:19 +0300

    Better nvm installation script

--- install/nvm.sh
+++ install/nvm.sh
@@ -1,5 +1,7 @@
 
 if [ ! -d "$HOME/.nvm" ]; then
   git clone https://github.com/creationix/nvm.git $HOME/.nvm
-  cd $HOME/.nvm && git checkout v0.33.11
+  cd $HOME/.nvm && git checkout v0.34.0
+else
+  cd $HOME/.nvm && git fetch --tags && git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
 fi
