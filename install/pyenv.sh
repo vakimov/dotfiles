7f7b9491da3781396334743bed70b2c4c1e5af20
Victor Akimov <akimov.victor@gmail.com>
2019-08-12 08:55:22 +0300

    Add dependencies for pyenv

--- install/pyenv.sh
+++ install/pyenv.sh
@@ -17,4 +17,5 @@ fi
 
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
     libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
-    libncursesw5-dev xz-utils
+    libncursesw5-dev xz-utils libffi-dev
+
